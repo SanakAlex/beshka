2e71e13892df5af2158981ff9f04f15612e88462
Alex Sanak <sanak.alex@gmail.com>
2017-10-22 23:34:39 +0300

    CORS /login works

--- src/main/java/com/creato/beshka/config/AppConfiguration.java
+++ src/main/java/com/creato/beshka/config/AppConfiguration.java
@@ -3,6 +3,9 @@ package com.creato.beshka.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfiguration {
@@ -10,5 +13,16 @@ public class AppConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
--- src/main/java/com/creato/beshka/config/SecurityConfiguration.java
+++ src/main/java/com/creato/beshka/config/SecurityConfiguration.java
@@ -13,11 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,11 +21,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     private final UserRepository userRepository;
     final private CustomUserDetailsService userDetailsService;
+    private final CORSFilter corsFilter;
 
     @Autowired
-    public SecurityConfiguration(UserRepository userRepository, CustomUserDetailsService userDetailsService) {
+    public SecurityConfiguration(UserRepository userRepository, CustomUserDetailsService userDetailsService, CORSFilter corsFilter) {
         this.userRepository = userRepository;
         this.userDetailsService = userDetailsService;
+        this.corsFilter = corsFilter;
     }
 
     @Override
@@ -49,6 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(corsFilter, ChannelProcessingFilter.class)
                 .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/", "/static/**", "/favicon.ico").permitAll()
@@ -82,13 +81,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
