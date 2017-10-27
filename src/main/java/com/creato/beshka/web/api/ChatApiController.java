9cecfa765a7fd5060c10f62dad9aa97ecdb7bcec
Alex Sanak <sanak.alex@gmail.com>
2017-10-27 17:48:05 +0300

    web sockets test

--- src/main/java/com/creato/beshka/web/api/ChatApiController.java
+++ src/main/java/com/creato/beshka/web/api/ChatApiController.java
@@ -10,6 +10,9 @@ import com.creato.beshka.services.IChatService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -78,5 +81,28 @@ public class ChatApiController {
     @JsonView(View.WithMessages.class)
     public ChatDto getChatWithUser(@PathVariable Long id) throws NoSuchEntityException, AuthRequiredException {
         return chatService.getChatWithUser(id);
+    }
+
+    @MessageMapping("/{id}/post")
+    @SendTo("/topic/chats/{id}")
+    public MessageDto send(@DestinationVariable("id") Long chatId, MessageDto messageDto)
+            throws NoSuchEntityException, InputErrorException {
+        return chatService.postMessage(messageDto);
+    }
+
+    @MessageMapping("/{id}")
+    @SendTo("/topic/chats/{id}")
+    public ChatDto getChatWS(@DestinationVariable("id") Long chatId)
+            throws NoSuchEntityException, InputErrorException {
+        return chatService.getChatById(chatId);
+    }
+
+    @MessageMapping
+    @SendTo("/topic/chats")
+    public List<ChatDto> getChatsWS(
+            @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
+            @RequestParam(value = "limit", required = false, defaultValue = "10") int limit
+    ) throws NoSuchEntityException, AuthRequiredException {
+        return chatService.getChatsOfCurrentUser(offset, limit);
     }
 }
