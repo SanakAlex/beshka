24b03c4f4fe91e40f48d4847640282a219d3fbcc
Alex Sanak <sanak.alex@gmail.com>
2017-11-02 15:02:40 +0200

    web sockets test №2

--- src/main/java/com/creato/beshka/web/api/ChatApiController.java
+++ src/main/java/com/creato/beshka/web/api/ChatApiController.java
@@ -3,6 +3,7 @@ package com.creato.beshka.web.api;
 import com.creato.beshka.converters.View;
 import com.creato.beshka.converters.dto.ChatDto;
 import com.creato.beshka.converters.dto.MessageDto;
+import com.creato.beshka.converters.dto.UserDto;
 import com.creato.beshka.exceptions.AuthRequiredException;
 import com.creato.beshka.exceptions.InputErrorException;
 import com.creato.beshka.exceptions.NoSuchEntityException;
@@ -12,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin
@@ -23,10 +27,12 @@ import java.util.List;
 public class ChatApiController {
 
     private final IChatService chatService;
+    private final SimpMessagingTemplate template;
 
     @Autowired
-    public ChatApiController(IChatService chatService) {
+    public ChatApiController(IChatService chatService, SimpMessagingTemplate template) {
         this.chatService = chatService;
+        this.template = template;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -84,25 +90,34 @@ public class ChatApiController {
     }
 
     @MessageMapping("/{id}/post")
-    @SendTo("/topic/chats/{id}")
-    public MessageDto send(@DestinationVariable("id") Long chatId, MessageDto messageDto)
+    @JsonView(View.WithoutMessages.class)
+    public void send(@DestinationVariable("id") Long chatId, @Payload MessageDto messageDto)
             throws NoSuchEntityException, InputErrorException {
-        return chatService.postMessage(messageDto);
+        MessageDto newMessageDto = chatService.postMessage(messageDto);
+        template.convertAndSend("/queue/chats/" + chatId, newMessageDto);
+        ChatDto chatDto = chatService.getChatById(chatId);
+        for (UserDto user: chatDto.getMembers()
+             ) {
+            template.convertAndSendToUser(user.getEmail(), "/queue/chats", chatDto);
+        }
     }
 
-    @MessageMapping("/{id}")
-    @SendTo("/topic/chats/{id}")
-    public ChatDto getChatWS(@DestinationVariable("id") Long chatId)
+    @SubscribeMapping("/{id}")
+    @JsonView(View.WithMessages.class)
+    public void getChatWS(@DestinationVariable("id") Long chatId, Principal principal)
             throws NoSuchEntityException, InputErrorException {
-        return chatService.getChatById(chatId);
+        ChatDto chatDto =  chatService.getChatById(chatId);
+        template.convertAndSendToUser(principal.getName(), "/queue/chats/" + chatId, chatDto);
     }
 
-    @MessageMapping
-    @SendTo("/topic/chats")
-    public List<ChatDto> getChatsWS(
+    @SubscribeMapping
+    @JsonView(View.WithoutMessages.class)
+    public void getChatsWS(
             @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
-            @RequestParam(value = "limit", required = false, defaultValue = "10") int limit
+            @RequestParam(value = "limit", required = false, defaultValue = "10") int limit,
+            Principal principal
     ) throws NoSuchEntityException, AuthRequiredException {
-        return chatService.getChatsOfCurrentUser(offset, limit);
+        List<ChatDto> chatDtos = chatService.getChatsOfCurrentUser(offset, limit);
+        template.convertAndSendToUser(principal.getName(), "/queue/chats", chatDtos);
     }
 }
